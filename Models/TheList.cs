c14a04d645afc65b3354238dda299821d3405326
Lisa <lisajbalbach@gmail.com>
2020-09-19 20:09:40 -0400

    Add project files.

---
+++ Models/TheList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class TheList
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+
+        [Display(Name = "Created Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
+    }
+}
